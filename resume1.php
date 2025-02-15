226350cad242537d63da3d0f1e3d047d25581613
JOHNMARK60 <johnmarkyecyec18@gmail.com>
2025-02-15 08:04:27 +0800

    create

---
+++ resume1.php
@@ -0,0 +1,68 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Julius+Sans+One&family=Open+Sans&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+<page size="A4">
+    <div class="container">
+        <div class="leftPanel">
+            <img src="MARK.jpg" alt="Profile Picture"/>
+            <div class="details">
+                <div class="item bottomLineSeparator">
+                    <h2>CONTACT</h2>
+                    <div class="smallText">
+                        <p><i class="fa fa-phone contactIcon"></i>0985125281</p>
+                        <p><i class="fa fa-envelope contactIcon"></i> <a href="mailto:lorem@ipsum.com">johnmarkyecyec18@gmail.com</a></p>
+                        <p><i class="fa fa-map-marker contactIcon"></i> Sta Cruz Bontoc so.leyte</p>
+                        <p><i class="fa fa-facebook-square contactIcon"></i> <a href="#">John Mark Yecyec</a></p>
+                        <p><i class="fa fa-instagram contactIcon"></i> <a href="#">Yecyec JM</a></p>
+
+                    </div>
+                </div>
+                <div class="item bottomLineSeparator">
+                    <h2>SKILLS</h2>
+                    <div class="smallText">
+                        <div class="skill"><span>Basic Computer Skills </span><span class="yearsOfExperience">Using Microsoft Office (Word, Excel, PowerPoint) or Google Suite.</span></div>
+                        <div class="skill"><span>Word</span><span class="yearsOfExperience">3 years</span></div>
+                        <div class="skill"><span>PowerPoint</span><span class="yearsOfExperience">1 year</span></div>
+                    </div>
+                </div>
+                <div class="item">
+                    <h2>EDUCATION</h2>
+                    <p class="bolded white">Bachelor in Science in information technology</p>
+                    <p>Southern Leyte State University</p>
+                    <p>Still Studying</p>
+                </div>
+            </div>
+        </div>
+        <div class="rightPanel">
+            <h1>JOHN MARK YECYEC</h1>
+            <h3>STUDENT</h3>
+            <h2>About Me</h2>
+            <p>I am a hardworking and dedicated individual with strong multitasking skills. I can efficiently manage multiple tasks while maintaining focus and quality. I am also a well-mannered and respectful person who values teamwork, professionalism, and continuous learning. I am eager to apply my skills and grow in any opportunity that comes my way.</p>
+            <h2>Work Experience</h2>
+            <ul>
+                <li><span class="bolded"></span> |</li>
+                <li><span class="bolded"></span> |</li>
+                <li><span class="bolded"></span> |</li>
+            </ul>
+        </div>
+    </div>
+</page>
+
+</body>
+</html>
